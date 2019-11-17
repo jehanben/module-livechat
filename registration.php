398e44828fabdd68d74485296cfb4f39ce453a9f
Jehan Ryan <ryan.jehan@gmail.com>
2019-11-17 11:31:34 +0530

    Craeted module generation files

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ * @category    Aligent
+ * @package     Aligent_LiveChat
+ * @author      Jehan Ryan <ryan.jehan@gmail.com>
+ *
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Aligent_LiveChat',
+    __DIR__
+);
