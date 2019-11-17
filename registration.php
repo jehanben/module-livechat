f48bf53a11d3d5c520699f4559f3eb84cd86a2a4
Jehan Ryan <ryan.jehan@gmail.com>
2019-11-17 11:37:31 +0530

    Updated the registraion file

--- registration.php
+++ registration.php
@@ -1,11 +1,5 @@
 <?php
-/**
- *
- * @category    Aligent
- * @package     Aligent_LiveChat
- * @author      Jehan Ryan <ryan.jehan@gmail.com>
- *
- */
+
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Aligent_LiveChat',
